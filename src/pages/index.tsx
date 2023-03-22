6b72a2e58e7fedfdbfcbec1cc742e66497f42d24
Renato <renatomarquesteles@gmail.com>
2023-03-22 16:58:36 -0300

    feat: adds Home strutcture

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,29 @@
+import Image from 'next/image';
+
+import shirt1 from '../assets/shirts/1.png';
+import shirt2 from '../assets/shirts/2.png';
+// import shirt3 from '../assets/shirts/3.png';
+import { HomeContainer, Product } from '../styles/pages/home';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <HomeContainer>
+      <Product>
+        <Image src={shirt1} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Shirt X</strong>
+          <span>$ 19.90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={shirt2} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Shirt X</strong>
+          <span>$ 19.90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   );
 }
