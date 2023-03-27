e460ee7a9659b82299124772d05ce75814ff5e4d
Renato <renatomarquesteles@gmail.com>
2023-03-27 20:53:31 -0300

    feat: adds navigation to products page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import 'keen-slider/keen-slider.min.css';
 import { stripe } from '../lib/stripe';
 import { GetStaticProps } from 'next';
 import Stripe from 'stripe';
+import Link from 'next/link';
 
 interface HomeProps {
   products: {
@@ -28,14 +29,16 @@ export default function Home({ products }: HomeProps) {
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map((product) => (
-        <Product key={product.id} className="keen-slider__slide">
-          <Image src={product.imageUrl} alt="" width={520} height={480} />
+        <Link href={`/product/${product.id}`} key={product.id}>
+          <Product className="keen-slider__slide">
+            <Image src={product.imageUrl} alt="" width={520} height={480} />
 
-          <footer>
-            <strong>{product.name}</strong>
-            <span>$ {product.price.toFixed(2)}</span>
-          </footer>
-        </Product>
+            <footer>
+              <strong>{product.name}</strong>
+              <span>$ {product.price.toFixed(2)}</span>
+            </footer>
+          </Product>
+        </Link>
       ))}
     </HomeContainer>
   );
