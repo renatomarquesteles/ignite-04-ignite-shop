29dba373db07b2ba248b5119dd6a277283a0aba0
Renato <renatomarquesteles@gmail.com>
2023-03-27 21:05:01 -0300

    styles: adds product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,11 +1,30 @@
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '@/src/styles/pages/product';
 import { useRouter } from 'next/router';
 
 export default function Product() {
   const { query } = useRouter();
 
   return (
-    <div>
-      <h1>Product: {query.id}</h1>
-    </div>
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Shirt X</h1>
+        <span>$ 19.90</span>
+
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque in
+          placeat velit debitis fuga reiciendis ab, aut explicabo deserunt
+          quisquam esse at et! Eius laboriosam reprehenderit voluptatem at
+          inventore quis?
+        </p>
+
+        <button>Order now</button>
+      </ProductDetails>
+    </ProductContainer>
   );
 }
