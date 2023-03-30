48d4d5a925dd8d47042ac48e58b2ec5e7a3a0ceb
Renato <renatomarquesteles@gmail.com>
2023-03-30 01:35:44 -0300

    styles: add success page styling

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,3 +1,19 @@
+import Link from 'next/link';
+import { ImageContainer, SuccessContainer } from '../styles/pages/success';
+
 export default function Success() {
-  return <h1>Success</h1>;
+  return (
+    <SuccessContainer>
+      <h1>Order completed!</h1>
+
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Nice, <b>[User]</b>! the <b>[Shirt]</b> will be shipped to your home
+        soon!
+      </p>
+
+      <Link href="/">Back to catalog </Link>
+    </SuccessContainer>
+  );
 }
