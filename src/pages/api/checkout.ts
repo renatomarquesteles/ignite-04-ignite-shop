8838acd5248b3f14d4ee0e3288e19b164e6d0eef
Renato <renatomarquesteles@gmail.com>
2023-03-30 05:46:18 -0300

    fix: changed api routes to pages/api

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { stripe } from '../lib/stripe';
+import { stripe } from '../../lib/stripe';
 
 export default async function handler(
   req: NextApiRequest,
@@ -15,7 +15,7 @@ export default async function handler(
     return res.status(400).json({ error: 'Price id not found' });
   }
 
-  const successUrl = `${process.env.NEXT_URL}/success`;
+  const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`;
   const cancelUrl = `${process.env.NEXT_URL}/`;
 
   const checkoutSession = await stripe.checkout.sessions.create({
