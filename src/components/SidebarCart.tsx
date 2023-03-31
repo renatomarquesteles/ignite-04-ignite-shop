dd5cc7942f11b6d0115a3f24772d66da0b5cf8c5
Renato <renatomarquesteles@gmail.com>
2023-03-31 04:14:17 -0300

    styles: add Sidebar Cart styling

--- src/components/SidebarCart.tsx
+++ src/components/SidebarCart.tsx
@@ -1,4 +1,17 @@
-import { SidebarContainer } from '../styles/components/SidebarCart';
+import { X } from '@phosphor-icons/react';
+
+import {
+  CheckoutButton,
+  ProductBody,
+  ProductContainer,
+  ProductImageContainer,
+  ProductsList,
+  QuantityContainer,
+  RemoveButton,
+  SidebarContainer,
+  SidebarContent,
+  TotalPriceContainer,
+} from '../styles/components/SidebarCart';
 
 interface SidebarCartProps {
   isOpen: boolean;
@@ -6,10 +19,48 @@ interface SidebarCartProps {
 }
 
 export default function SidebarCart({ isOpen, close }: SidebarCartProps) {
+  const products = [1, 2, 3];
+
   return (
     <SidebarContainer isOpen={isOpen}>
-      <h1>SidebarCart</h1>
-      <button onClick={close}>X</button>
+      <header>
+        <button onClick={close}>
+          <X weight="bold" size="1.5rem" color="#8D8D99" />
+        </button>
+      </header>
+
+      <SidebarContent>
+        <h1>Shopping Bag</h1>
+
+        <ProductsList>
+          {products.map((product) => (
+            <ProductContainer key={product}>
+              <ProductImageContainer></ProductImageContainer>
+              <ProductBody>
+                <p>Beyond the Limits T-Shirt</p>
+                <span>$ 19.90</span>
+                <RemoveButton>Remove</RemoveButton>
+              </ProductBody>
+            </ProductContainer>
+          ))}
+        </ProductsList>
+
+        <footer>
+          <QuantityContainer>
+            <span>Quantity</span>
+            <span>3 items</span>
+          </QuantityContainer>
+
+          <TotalPriceContainer>
+            <span>Total price</span>
+            <span>$ 120.00</span>
+          </TotalPriceContainer>
+
+          <CheckoutButton disabled={products.length === 0}>
+            Checkout
+          </CheckoutButton>
+        </footer>
+      </SidebarContent>
     </SidebarContainer>
   );
 }
--- src/styles/components/SidebarCart.ts
+++ src/styles/components/SidebarCart.ts
@@ -7,6 +7,8 @@ export const SidebarContainer = styled('div', {
   height: '100%',
   width: '100%',
   maxWidth: '30rem',
+  display: 'flex',
+  flexDirection: 'column',
   background: '$gray800',
   transform: 'translateX(110%)',
   transition: 'all 0.2s ease-in-out',
@@ -17,5 +19,147 @@ export const SidebarContainer = styled('div', {
         transform: 'translateX(0%)',
       },
     },
+  },
+
+  header: {
+    padding: '1.5rem',
+    display: 'flex',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+
+    button: {
+      border: 'none',
+      background: 'transparent',
+      cursor: 'pointer',
+    },
+  },
+});
+
+export const SidebarContent = styled('div', {
+  padding: '0 3rem 3rem',
+  display: 'flex',
+  flexDirection: 'column',
+  flex: 1,
+
+  h1: {
+    fontSize: '1.25rem',
+    fontWeight: 'bold',
+    color: '$gray100',
+    lineHeight: '160%',
+  },
+});
+
+export const ProductsList = styled('ul', {
+  margin: '2rem 0',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '1.5rem',
+  flex: 1,
+  listStyle: 'none',
+  maxHeight: 'calc(100vh - 24rem)',
+  overflow: 'auto',
+});
+
+export const QuantityContainer = styled('div', {
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+
+  span: {
+    color: '$gray100',
+
+    '&:last-child': {
+      fontSize: '1.125rem',
+    },
+  },
+});
+
+export const TotalPriceContainer = styled('div', {
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  marginTop: '0.25rem',
+
+  span: {
+    color: '$gray100',
+    fontSize: '1.125rem',
+    fontWeight: 'bold',
+
+    '&:last-child': {
+      fontSize: '1.5rem',
+    },
+  },
+});
+
+export const CheckoutButton = styled('button', {
+  width: '100%',
+  borderRadius: 8,
+  background: '$green500',
+  border: 'none',
+  padding: '1.25rem 0.5rem',
+  textAlign: 'center',
+  fontSize: '1.125rem',
+  fontWeight: 'bold',
+  color: '$white',
+  marginTop: '3.4rem',
+  cursor: 'pointer',
+  transition: 'all 0.3s',
+
+  '&:disabled': {
+    opacity: 0.6,
+    cursor: 'not-allowed',
+  },
+
+  '&:not(:disabled):hover': {
+    backgroundColor: '$green300',
+  },
+});
+
+export const ProductContainer = styled('li', {
+  display: 'flex',
+  gap: '1.25rem',
+});
+
+export const ProductImageContainer = styled('div', {
+  width: '6.25rem',
+  height: '5.75rem',
+  background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
+  borderRadius: 8,
+  display: 'grid',
+  placeItems: 'center',
+});
+
+export const ProductBody = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+
+  p: {
+    fontSize: '1.125rem',
+    color: '$gray300',
+    lineHeight: '160%',
+  },
+
+  span: {
+    marginTop: '0.125rem',
+    fontWeight: 'bold',
+    fontSize: '1.125rem',
+    color: '$gray100',
+    lineHeight: '160%',
+  },
+});
+
+export const RemoveButton = styled('button', {
+  width: 'fit-content',
+  marginTop: '0.5rem',
+  fontSize: '1rem',
+  fontWeight: 'bold',
+  color: '$green500',
+  background: 'transparent',
+  border: 'none',
+  cursor: 'pointer',
+  transition: 'color 0.3s',
+
+  '&:not(:disabled):hover': {
+    color: '$green300',
   },
 });
